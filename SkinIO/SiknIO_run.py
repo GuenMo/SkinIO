3d2697e653c9fe4991b5ab3b164c255e7946950a
GuenMo Kim <hybrid0815@gmail.com>
2016-11-03 02:05:52 +0900

    [add] 프로젝트 생성

---
+++ SkinIO/SiknIO_run.py
@@ -0,0 +1,18 @@
+# coding:utf-8
+
+def run():
+	try:
+		filePath = __file__
+		appPath = filePath.rpartition("\\")[0]
+	except:
+		print "Application`s path not exist."
+
+	else:
+		import sys
+		path = appPath
+
+		if not path in sys.path:
+			sys.path.append(path)
+
+		import skinIO 
+		skinIO.show()
